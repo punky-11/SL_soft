bbe186732a6bf50c907d117b75e783fad53b74d8
santiago <shernandez998@misena.edu.co>
2023-10-08 11:04:01 -0500

    creacion de swagger

--- backend/controller/use-cases/use.usuario.js
+++ backend/controller/use-cases/use.usuario.js
@@ -5,20 +5,19 @@ const datosCliente = require('../data-access/date.clientes');
 
 exports.login = async (req, res) => {
   try {
-    const correoElectronico = ({email: req.body.email, password: req.body.password})
+    const correoElectronico = ({email: req.body.email, password: req.body.password});
     const validarCorreo = await datosUsuario.buscarUsuario(correoElectronico);
     if (validarCorreo) {
-      const clienteCorreo = await datosCliente.buscarClientes({ email :correoElectronico.email});
+      const clienteCorreo = await datosCliente.buscarClientes({email: correoElectronico.email});
       console.log(clienteCorreo);
       if (clienteCorreo) {
-        const datos = await clienteCorreo({"_id":0});
+        const datos = await clienteCorreo({'_id': 0});
         res.status(200).json(datos);
       } else {
         res.status(500).json({message: 'El cliente no se encontro'});
       }
     } else {
       res.status(500).json({message: 'El usuario no existe'});
-  
     };
   } catch (error) {
     console.log(error);
--- backend/routes/ruta.js
+++ backend/routes/ruta.js
@@ -10,35 +10,78 @@ router.get('/buscarProductos', productos.buscarProductos);
 router.post('/actualizarProductos/:id', productos.actualizarProductos);
 router.post('/eliminarProductos/:id', productos.eliminarProductos);
 // cliente
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *   cliente:
+ *     type: object
+ *     properties:
+ *       nombre:
+ *         type: String,
+
+  apellido:
+    type: String,
+  documento:
+    type: String,
+
+  celular:
+    type: String,
+
+  email:
+    type: String,
+
+  direccion:
+    type: String,
+ */
 router.post('/agregarCliente', cliente.agregarClientes);
 router.get('/buscarCliente', cliente.buscarClientes);
 router.post('/eliminarClientes/:id', cliente.eliminarClientes);
 router.post('/actualizarClientes/:id', cliente.actualizarClientes);
 // usuario
 /**
+* @swagger
+* components:
+* schemas:
+* login:
+* type: object
+* propieties:
+* correo:
+* type: string
+* description: el correo del usuario
+* contrasena:
+* type: string
+* description: contrasena del usuario
+* rol:
+* type: string
+* description: rol de usuario, sea cliente o tattoo artist
+* required:
+* - correo
+* - contrasena
+* - rol
+* example:
+* correo: alan@gmail.com
+* contrasena: 123456789
+*       rol: cliente
+*
+*/
+/**
  * @swagger
- * components:
- *  schemas:
- *    User:
- *      type: object
- *      propieties:
- *        correo:
- *          type: string
- *          description: el correo del usuario
- *        contrasena:
- *          type: string
- *          description: contrasena del usuario
- *        rol:
- *          type: string
- *          description: rol de usuario, sea cliente o tattoo artist
- *      required:
- *        - correo
- *        - contrasena
- *        - rol
- *      example:
- *        correo: alan@gmail.com
- *        contrasena: 123456789
- *        rol: cliente
+ * /v1/login:
+ *  post:
+ *    summary: ingreso al login
+ *    tags: [login]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *      application/json:
+ *        schema:
+ *          type: object
+ *          $ref: '#/components/schemas/login'
+ *    responses:
+ *      200:
+ *        description: ingreso corectamente
+ *
  *
  */
 
